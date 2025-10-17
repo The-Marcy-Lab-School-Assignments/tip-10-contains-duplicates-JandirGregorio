7f44a417b46ee7dff85a22cbf81123bc03a696b7
Jandir <jandirgregorio@icloud.com>
2025-10-17 10:28:29 -0400

    completed code challenge

--- index.js
+++ index.js
@@ -13,6 +13,47 @@
 
 const containsDuplicate = (nums) => {
   //write your code here
+  //traverse through the array using a foor loop
+  //build a freq table { number: frequencyCounty}
+  //do another for loop to see how many times each value has appeared
+    //if the number one number has a freq of 2 or more, return true
+    // otherwise, return false
+
+    //create freq object to store the frequency of each number
+    //if the array is empty return false
+    if(nums.length <= 1){
+      return false;
+    }
+    const freq = {};
+
+    // iterate throught the aray and build freq table
+    for(let i = 0; i < nums.length; i++){
+      // get each key (number) from the array and store it in a variable
+      const number = nums[i];
+      // check if the number exists in the freq object
+      // if it doesn't exist
+      if(freq[number] === undefined){
+        // initialize the number to 1
+        freq[number] = 1;
+      } else{
+        // otherwise, increment its count
+        freq[number]++;
+      }
+    }
+    //now that the freq object is built, traverse once again through nums 
+    //create variable to store how many numbers repeat more than once
+    let countDuplicates = 0;
+    for(let i = 0; i < nums.length; i++){
+      //store each value in arrays in `num`
+      const num = nums[i];
+      if(freq[num] > 1){
+        // keep track of the all the numbers that repeat more than once
+        countDuplicates++;
+      } 
+    }
+    //if we have at leats one number that repeats more than once, return true, false otherwise
+    return countDuplicates >= 1 ? true : false;
+
 };
 
 // Export the function for testing
